e60aea11a075eee36eaf3208fd02f1a472ebe9db
Teerth Patel <teerth1205@gmail.com>
2020-02-08 19:46:01 +0000

    first commit

---
+++ lib/new/cli.rb
@@ -0,0 +1,8 @@
+require "new/cli/version"
+
+module New
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/new/cli/version.rb
@@ -0,0 +1,5 @@
+module New
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
